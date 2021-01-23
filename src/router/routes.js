ee5069268665689c0011c5349ec1584ef5d0d0f6
Alberto De Agostini <albertodeagostini88@gmail.com>
2021-01-23 17:19:25 +0100

    remove not-found route + always show home view

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,13 +1,9 @@
 import Home from '../views/home/Home.vue'
-import NotFound from '../views/NotFound.vue'
 
 export let routes = [{
-    path: '/',
+    path: '/:path(.*)',
     component: Home,
     meta: { 
         title: 'Home'
     }
-}, {
-    path: '/:path(.*)',
-    component: NotFound
 }]
